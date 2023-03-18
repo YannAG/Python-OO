1cf0a6335d2350316b8582f9b5a5a4e92eefb19f
YannAG <yan.boat12@gmail.com>
2023-03-18 14:00:33 -0300

    Atividade PythonOO

---
+++ Computador.py
@@ -0,0 +1,9 @@
+class Computador:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def ligar(self):
+        print(f'Ligando o {self.marca} modelo {self.modelo}')
+
+
